07ec227b8a79cd074e929157b8715c8db28fca41
Denis Belous <project.bdv@gmail.com>
2018-09-04 15:54:33 +0300

    Add files via upload

--- l1.go
+++ l1.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 	"time"
 )
-
-func main() {
+func main () {
 	var buf0 []int
-	for {
+		for {
 		buf0 = append(buf0, 1, 2, 3, 4, 5)
-		time.After(10*time.Second)
+		//time.After(10 * time.Second)
+		time.Sleep(5*time.Second)
 		fmt.Println(buf0)
 	}
 }
+/* func main ()  {
 
-//func aa ()  {
-//	for  {
-//
-//	}
-//}
+	go m( )
+	var input  string
+	fmt.Scanln(&input)
+}
+*/
