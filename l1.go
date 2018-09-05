8ac4a74f9a0dc9cfdf9c9d3c8d31e78408ca7135
bdv <projectbdv>
2018-09-05 16:02:21 +0300

    add gosched add pproff

--- l1.go
+++ l1.go
@@ -3,22 +3,27 @@ package main
 import (
 	"fmt"
 	"time"
+	"runtime"
+	_ "net/http/pprof"
+	"github.com/pkg/profile"
 )
 func m1 () {
 	var buf0 []int
 		for {
 		buf0 = append(buf0, 1, 2, 3, 4, 5)
 		//time.After(10 * time.Second)
-		time.Sleep(5*time.Second)
+		time.Sleep(2*time.Second)
 		fmt.Println(buf0)
+		runtime.Gosched()
 	}
 }
  func main ()  {
-	 for i:=0;i <10 ;i++ {
+	 for i:=0;i <100 ;i++ {
 		 go m1()
 	 }
+	 defer profile.Start().Stop()
 	 var buf int
 	fmt.Scanln(&buf)
-}
+ }
 
 //test of test tett
